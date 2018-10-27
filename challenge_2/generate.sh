1777513431189cf9499f0e6911b94a41c3b1ade9
chebyrash <pd.stroganov@gmail.com>
2018-10-27 11:56:56 +0100

    Preserve minesweeper name by zipping it

--- challenge_2/generate.sh
+++ challenge_2/generate.sh
@@ -2,9 +2,9 @@
 
 cd home/ && ./compile.sh && rm -rf minesweeper.c compile.sh
 
-cat minesweeper >> mine.ico
+zip minesweeper.zip minesweeper && cat minesweeper.zip >> mine.ico
 
-rm -rf minesweeper
+rm -rf minesweeper minesweeper.zip
 
 export filename=$(env LC_CTYPE=C tr -dc "A-Z0-9" < /dev/urandom | head -c 16 | xargs)
 
